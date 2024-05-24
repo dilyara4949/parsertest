d18fb4715679c95b9763173fa45ae38a0717653c
dilya <mukhambetovad49@mail.ru>
2024-05-24 15:26:24 +0500

    add test for parser

---
+++ parser.go
@@ -0,0 +1,53 @@
+package assets
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type Product struct {
+	Name   string   `json:"name"`
+	Market string   `json:"market"`
+	Tags   []string `json:"tags"`
+}
+
+var (
+	errEmptyInput   = errors.New("empty input")
+	errManyElements = errors.New("input should contain 10 or less elements")
+)
+
+/*
+	Parse consumes a JSON with an array of products.
+	Example:
+	[
+	   {
+		  "name":"test name",
+		  "market":"USA",
+		  "tags":[
+			 "t1",
+			 "t2",
+		  ]
+	   }
+	]
+*/
+
+func Parse(input []byte) ([]Product, error) {
+	if input == nil || len(input) == 0 {
+		return nil, errEmptyInput
+	}
+
+	fmt.Println(len(input))
+
+	if len(input) > 10 {
+		return nil, errManyElements
+	}
+
+	var p []Product
+
+	if err := json.Unmarshal(input, &p); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
---
+++ parser_test.go
@@ -0,0 +1,48 @@
+package assets
+
+import (
+	"encoding/json"
+	"errors"
+	"reflect"
+	"testing"
+)
+
+type TestCase struct {
+	name    string
+	input   []byte
+	want    []Product
+	wantErr error
+}
+
+func InitData() []TestCase {
+	tests := make([]TestCase, 0)
+
+	tests = append(tests, TestCase{"nil array", nil, nil, errEmptyInput})
+
+	products := []Product{{}, {}, {}, {}, {}, {}, {}, {}, {}, {}}
+	data, _ := json.Marshal(products)
+
+	tests = append(tests, TestCase{"empty array", data, nil, errManyElements})
+
+	products = []Product{}
+	data, _ = json.Marshal(products)
+
+	tests = append(tests, TestCase{"valid", []byte(`[]`), products, nil})
+	return tests
+}
+
+func TestParse(t *testing.T) {
+	tests := InitData()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Parse(tt.input)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parse() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
